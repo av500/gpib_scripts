89d3e7549ede034d3efc7776a0457eeb1736795a
Vladimir Pantelic <vladimir@pantelic.net>
2023-05-10 12:02:55 +0200

    use supplied filename or create one. if hpgs is found, create PNG file
    Signed-off-by: Vladimir Pantelic <vladimir@pantelic.net>

--- scripts/hardcopy_FSEx_RS232_pyvisa.py
+++ scripts/hardcopy_FSEx_RS232_pyvisa.py
@@ -2,14 +2,12 @@
 import pyvisa as visa
 import sys
 from pyvisa import constants
+import os
+from datetime import datetime
 
 def print_hex(data):
 	hex_str = ":".join("{:02x}".format(c) for c in data)
 	print(hex_str + " | " + str(data) )
-
-if len(sys.argv) < 2 :
-	print("usage: hardcopy_FSEx filename.png")
-	exit();
 
 rm = visa.ResourceManager('@py')
 
@@ -35,15 +33,44 @@ print(instr.query("*IDN?"))
 print(instr.query(":HCOP:DEST1?"))
 print(instr.query(":HCOP:DEV:LANG1?"))
 
-#exit()
+tmp_path = "__temp.hpgl"
 
-filepath = sys.argv[1]
-f = open(filepath, "wb")
+cmd = "which hpgs >/dev/null 2>&1"
+res = os.system(cmd)
+
+if res :
+	print("'hpgs' not found, creating HPGL file")
+	has_hpgs = 0
+else :
+	print("'hpgs' found, creating PNG file")
+	has_hpgs = 1
+	
+if len(sys.argv) < 2 :
+	now = datetime.now()
+	if has_hpgs :
+		filepath = now.strftime("FSEx_%Y%m%d_%H%M%S.png")
+	else :
+		filepath = now.strftime("FSEx_%Y%m%d_%H%M%S.hpgl")
+else :
+	filepath = sys.argv[1]
+	
+print("[file] " + filepath)	
+
+f = open(tmp_path, "wb")
+
+# force pen colors to match FSEB screen colors
+f.write(b'PC 1, 128, 128, 128\n');
+f.write(b'PC 2, 255, 255,   0\n');
+f.write(b'PC 3,   0, 255,   0\n');
+f.write(b'PC 5,   0, 255,   0\n');
+f.write(b'PC 6,   0,   0, 255\n');
 
 instr.write(":HCOPY:IMM")
 
 instr.read_termination = None
 instr.end_input = False
+
+size = 0
 
 while 1:
 	try:
@@ -54,13 +81,26 @@ while 1:
 		if data == b'\r' :
 			continue
 		f.write(data)
-		size = f.tell()
+		size = size + 1
 		if  size > 1 :
 			instr.timeout = 1000
-		if (size % 256) == 0 :
-			print(size)
+		if (size % 16) == 0 :
+			print("bytes read: "+ str(size), end='\r')
 		
-print("TOTAL SIZE: " + str(f.tell()))
+print("TOTAL SIZE: " + str(size))
 
 f.close()
+
+if has_hpgs :
+	cmd = "hpgs -r 150 -w 0.2  --no-linetype --darkmode --paper=A4l -d png_256 -o {0} {1}".format(filepath, tmp_path)
+	print(cmd)
+	os.system(cmd)
+	cmd = "rm {0}".format(tmp_path)
+	os.system(cmd)
+else :
+	cmd = "mv {0} {1}".format(tmp_path, filepath)
+	print(cmd)
+	os.system(cmd)
+
+exit()
 
