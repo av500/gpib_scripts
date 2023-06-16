6a18d5d2ae5184120e6fe8250be813665c88e05e
vladimir <vladimir@pantelic.net>
2023-06-16 21:44:11 +0200

    clean up and allow to pass the start note on the command line
    Signed-off-by: vladimir <vladimir@pantelic.net>

--- scripts/play9111A_pyvisa.py
+++ scripts/play9111A_pyvisa.py
@@ -4,6 +4,8 @@ import pyvisa as visa
 import time
 import sys
 
+start_note = 0
+
 def parse(file):
 	c = MIDIFile(file)
 	c.parse()
@@ -19,19 +21,39 @@ def parse(file):
 	ev = next(tit)
 	start = 0
 	dur   = 0
+	wait  = 0
+	last  = 0
+	factor = 1.2
+	
 	while ev :
 		if "End Of Track" in str(ev.message) :
 			break
-		time = ev.time
-		if ev.command == 0x80 :
-			dur = time - start;
-			print("time:" + str(start) + " dur:" + str(dur) + " note:" + str(note) + " vel:" + str(vel) )
-#			print("OFF :" + str(dur) + "   " + str(ev.data[0]) )
-		elif ev.command == 0x90 :
-			start = time;
+		now = ev.time
+
+		if ev.command == 0x90 :
+			wait  = now - start
+			start = now
 			note  = ev.data[0]
 			vel   = ev.data[1]
-#			print("ON  :" + str(start) + "   " + str(ev.data[0]) + " : "+ str(ev.data[1])) 
+
+		elif ev.command == 0x80 :
+			if wait == start :
+				wait = 0
+			dur = now - start;
+			print("time:" + str(start) + " wait:" + str(wait) + " dur:" + str(dur) + " note:" + str(note) + " vel:" + str(vel) )
+			
+			if vel >= 100 :
+				v = 4
+			else :
+				v = 3
+			d = int(dur * factor)	
+			cmd = "BP" + str(note - start_note) + "," + str(d) + "," + str(v)
+			#print(cmd)
+			instr.write(cmd)
+
+			time.sleep(factor * (wait - last)/1000)
+			last = dur
+			
 		ev = next(tit)
 	exit()
 
@@ -39,9 +61,6 @@ def parse(file):
 		track.parse()
 		print(f'Track {idx}:')
 		print(str(track))
-
-parse(sys.argv[1])
-exit()
 
 rm = visa.ResourceManager('@py')
 
@@ -52,11 +71,20 @@ instr.timeout = 5000
 instr.write("OI")
 print("device: " + instr.read())
 
+#instr.write("BP0,100,4")
+
 if len(sys.argv) < 2 :
-	print("usage: play9111 filename.mid")
+	print("usage: play9111a filename.mid [start_note]")
+	print("")
+	print("       we can omly play notes 0 - 48,")
+	print("       so [start_note] will be subtracted")
+	print("       from the MIDI note number")
+	
 	exit()
 
-print('playing: ', str(sys.argv[1]))
+if len(sys.argv) > 2 :
+	start_note = int(sys.argv[2])
+print("playing: " + str(sys.argv[1]) + "  from note: " + str(start_note))
 
 parse(sys.argv[1])
 
